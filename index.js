ef37f4b81aff261b01128c29548ee76adb6f2b6e
Thomas Greenstein <thomasgpw@gmail.com>
2019-02-24 16:25:41 -0500

    added text.json to compile text responses and read the json to the console in index.js' handlePostback method

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ const
   path = require('path'),
   body_parser = require('body-parser'),
   mongoose = require('mongoose'),
+  textResponses = require('./text')
   app = express().use(body_parser.json()); // creates express http server
 
 // Sets server port and logs message on success
@@ -145,20 +146,23 @@ function handleMessage(sender_psid, received_message) {
 }
 
 function handlePostback(sender_psid, received_postback) {
-  console.log('ok')
+  console.log(textResponses)
   console.log("handleMessage received_postback object", received_postback)
    let response;
   // Get the payload for the postback
   let payload = received_postback.payload;
 
-  // Set the response based on the postback payload
-  if (payload === 'yes') {
-    response = { "text": "Thanks!" }
-  } else if (payload === 'no') {
-    response = { "text": "Oops, try sending another image." }
-  }
-  // Send the message to acknowledge the postback
-  callSendAPI(sender_psid, response);
+  // if (payload === 'GREETING') {
+  //   response = { "text": "Hi, I'm Do Something Everyday.  What should I call you?"}
+  // }
+  // // Set the response based on the postback payload
+  // if (payload === 'yes') {
+  //   response = { "text": "Thanks!" }
+  // } else if (payload === 'no') {
+  //   response = { "text": "Oops, try sending another image." }
+  // }
+  // // Send the message to acknowledge the postback
+  // callSendAPI(sender_psid, response);
 }
 
 function callSendAPI(sender_psid, response) {
