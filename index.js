ca365126f444ef425d797ad023f828b36f94fbed
Thomas Greenstein <thomasgpw@gmail.com>
2019-03-08 16:42:49 -0500

    facing issue getting response object from inside updateStatus to inside callback for Mongoose.Model.findOneAndUpdate.  This attempt adds a parameter for findOneAndUpdate

--- index.js
+++ index.js
@@ -206,7 +206,7 @@ function updateStatus(sender_psid, status, callback, response){
   const options = {upsert: status === "INIT_0"};
   console.log("update status", status, response)
 
-  ChatStatus.findOneAndUpdate(query, update, options, callback).exec((err, cs) => {
+  ChatStatus.findOneAndUpdate(query, update, options, callback, response).exec((err, cs) => {
     console.log('update status to db: ', cs);
     callback(sender_psid, response);
   });
