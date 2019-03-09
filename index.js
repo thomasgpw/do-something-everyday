bd633b7d64a0c0cc4d4438a422ef0319ac292b3b
Thomas Greenstein <thomasgpw@gmail.com>
2019-03-09 18:02:01 -0500

    removed the dotenv component whichtripped up heroku

--- index.js
+++ index.js
@@ -22,7 +22,7 @@
  /*** GLOBAL CONSTANTS & REQUIREMENTS ***/
 
 'use strict';
-require('dotenv').config();
+// require('dotenv').config();
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const FACEBOOK_GRAPH_API_BASE_URL = 'https://graph.facebook.com/v2.6/';
 const MONGODB_URI = process.env.MONGODB_URI;
