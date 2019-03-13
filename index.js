5bbe42e070bb5e0ca959f8bf96dddc64368f79d8
Thomas Greenstein <thomasgpw@gmail.com>
2019-03-12 20:40:12 -0400

    specified against processing echo messages generated after calling SendAPI and changed the return objects inside of getStatus

--- index.js
+++ index.js
@@ -154,52 +154,54 @@ app.get('/:var(privacypolicy)?', (req, res) => {
 /**  CONTROLLER LOGIC **/
 
 function handleMessage(sender_psid, received_message) {
-  console.log("handleMessage received_message object", received_message)
-  const status = getStatus(sender_psid)
-  console.log(status, received_message.text)
-  // let response;
-  
-  // // Checks if the message contains text
-  // if (received_message.text) {    
-  //   // Create the payload for a basic text message, which
-  //   // will be added to the body of our request to the Send API
-  //   response = {
-  //     "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-  //   }
-  // } else if (received_message.attachments) {
-  //   // Get the URL of the message attachment
-  //   let attachment_url = received_message.attachments[0].payload.url;
-  //   response = {
-  //     "attachment": {
-  //       "type": "template",
-  //       "payload": {
-  //         "template_type": "generic",
-  //         "elements": [{
-  //           "title": "Is this the right picture?",
-  //           "subtitle": "Tap a button to answer.",
-  //           "image_url": attachment_url,
-  //           "buttons": [
-  //             {
-  //               "type": "postback",
-  //               "title": "Yes!",
-  //               "payload": "yes",
-  //             },
-  //             {
-  //               "type": "postback",
-  //               "title": "No!",
-  //               "payload": "no",
-  //             }
-  //           ],
-  //         }]
-  //       }
-  //     }
-  //   }
-  // } else {
-  //   throw "message held neither text nor attachments"
-  // }
-  
-  // // Send the response message
-  // callSendAPI(sender_psid, response);    
+  if (!received_message.is_echo) {
+    console.log("handleMessage received_message object", received_message)
+    const status = getDocument(sender_psid).status
+    console.log(status, received_message.text)
+    // let response;
+    
+    // // Checks if the message contains text
+    // if (received_message.text) {    
+    //   // Create the payload for a basic text message, which
+    //   // will be added to the body of our request to the Send API
+    //   response = {
+    //     "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+    //   }
+    // } else if (received_message.attachments) {
+    //   // Get the URL of the message attachment
+    //   let attachment_url = received_message.attachments[0].payload.url;
+    //   response = {
+    //     "attachment": {
+    //       "type": "template",
+    //       "payload": {
+    //         "template_type": "generic",
+    //         "elements": [{
+    //           "title": "Is this the right picture?",
+    //           "subtitle": "Tap a button to answer.",
+    //           "image_url": attachment_url,
+    //           "buttons": [
+    //             {
+    //               "type": "postback",
+    //               "title": "Yes!",
+    //               "payload": "yes",
+    //             },
+    //             {
+    //               "type": "postback",
+    //               "title": "No!",
+    //               "payload": "no",
+    //             }
+    //           ],
+    //         }]
+    //       }
+    //     }
+    //   }
+    // } else {
+    //   throw "message held neither text nor attachments"
+    // }
+    
+    // // Send the response message
+    // callSendAPI(sender_psid, response);    
+  }
 }
 
 function handlePostback(sender_psid, received_postback) {
@@ -259,14 +261,13 @@ function updateStatus(sender_psid, status) {
 
 function getStatus(sender_psid) {
   if (sender_psid != process.env.APP_PSID) {
-    let query = {user_id: sender_psid};
-    return ChatStatus.findOne(query, "status", (err, obj) => {
+    return ChatStatus.findOne({user_id: sender_psid}, {status: 1}, (err, obj) => {
       if(err) {
         throw err
       } else {
-        return obj.status
+        return obj
       }
-    })
+    }).status
     // console.log("Getting status", status)
     // return status;
   }
