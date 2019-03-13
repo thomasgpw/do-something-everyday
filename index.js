0aa42e4d95061516a9fc88f6c831a3b796278509
Thomas Greenstein <thomasgpw@gmail.com>
2019-03-12 20:59:37 -0400

    moved getStatus call

--- index.js
+++ index.js
@@ -156,8 +156,7 @@ app.get('/:var(privacypolicy)?', (req, res) => {
 function handleMessage(sender_psid, received_message) {
   if (!received_message.is_echo) {
     console.log("handleMessage received_message object", received_message)
-    const status = getStatus(sender_psid)
-    console.log(status, received_message.text)
+    console.log(getStatus(sender_psid), received_message.text)
     // let response;
     
     // // Checks if the message contains text
